a0f51ae92621d8c14a67934ae5fbc15362dabca0
Nguyễn Lê Phương Nam <henrynguyen1553@gmail.com>
2024-09-30 22:40:53 +0700

    test ci/cd

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div className='border-solid text-3xl text-green'>Home</div>
+    <div className="">
+      Home
+    </div>
   )
 }
