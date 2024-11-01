a0e8388631f46aa7752626bd5e2a9b262e6cca5e
HyyHyy <133102792+HyyHyyy@users.noreply.github.com>
2024-11-01 14:12:17 +0700

    Blog-Page

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -7,6 +7,7 @@ const path = {
   logout: '/logout',
   eventDetail: '/events/:id',
   loginOauthGoogle: '/login/oauth',
-  verify_account: '/verify-success'
+  verify_account: '/verify-success',
+  blog: '/blog-page'
 }
 export default path
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ import { ChangePassword, Home, LoginPage, ProfilePage, Register, ResetPassword, 
 // import path from "path";
 import Report from 'src/pages/staff/Report'
 import ForgotPasswordPage from 'src/pages/ForgotPasswordPage'
+import BlogPage from 'src/pages/BlogPage'
 
 type RouteType = {
   path: string
@@ -45,6 +46,10 @@ const publicRoutes: RouteType[] = [
   {
     path: '/reset-password',
     element: <ResetPassword />
+  },
+  {
+    path: '/blog-page',
+    element: <BlogPage />
   },
 ]
 
