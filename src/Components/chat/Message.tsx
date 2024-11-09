90ba43148375d04b703415deb4f77baae9a6b7ff
neru <ngocngocthuc@gmail.com>
2024-11-09 17:40:55 +0700

    Update Message.tsx

--- src/Components/chat/Message.tsx
+++ src/Components/chat/Message.tsx
@@ -7,20 +7,24 @@ interface Props {
   user: User
 }
 
-export default ({ message , user}: Props) => {
+export default ({ message, user }: Props) => {
   const isSentByMe = message.senderId === user.id
-  const time = message.createAt.seconds as number;
+  const time = message.createAt.seconds as number
   const createdAt = format(new Date(time * 1000), 'dd/MM/yyyy HH:mm')
   return (
-    <div className='p-2 justify-start items-start'>
-      <div className={`flex ${isSentByMe && 'flex-row-reverse'} justify-between items-center`}>
-        <div className={`flex ${isSentByMe && 'flex-row-reverse'} justify-center items-center`}>
-          <img src={message.senderAvatar || 'https://placehold.co/400'} alt='' className='w-6 h-6 rounded-full' />
-          <p className='text-sm mx-2'>{message.senderName}</p>
+    message && (
+      <div className='p-2 justify-start items-start'>
+        <div className={`flex ${isSentByMe && 'flex-row-reverse'} justify-between items-center`}>
+          <div className={`flex ${isSentByMe && 'flex-row-reverse'} justify-center items-center`}>
+            <img src={message.senderAvatar || 'https://placehold.co/400'} alt='' className='w-6 h-6 rounded-full' />
+            <p className='text-sm mx-2'>{message.senderName}</p>
+          </div>
+          <p className='text-sm text-white-normalActive'>{createdAt}</p>
         </div>
-        <p className='text-sm text-white-normalActive'>{createdAt}</p>
+        <p className={`text-xl p-2 bg-white-normalHover rounded-[0.5rem] text-black-darker mt-2 w-full text-wrap overflow-x-hidden ${isSentByMe && 'text-right'}`}>
+          {message.text}
+        </p>
       </div>
-      <p className={`text-xl p-2 bg-white-normalHover rounded-[0.5rem] text-black-darker mt-2 ${isSentByMe && 'text-right'}`}>{message.text}</p>
-    </div>
+    )
   )
 }
